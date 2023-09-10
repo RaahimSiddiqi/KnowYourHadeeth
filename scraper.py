85b4b530c960eca2710fe6c76354eda3ec780ad8
RaahimSiddiqi <k200146@nu.edu.pk>
2023-09-11 01:15:01 +0500

    Fixed various bugs related to invalid results, and skipping valid results

--- scraper.py
+++ scraper.py
@@ -9,16 +9,16 @@ from langchain.embeddings import HuggingFaceEmbeddings
 
 documents = []
 file = open("hadithscraper\hadithscraper\spiders\output.json", encoding="utf8")
-data = json.load(file)[:10]
+data = json.load(file)[7500:]
 
 for hadith in data:
     metadata = dict(narrator=hadith["narrator"], author=hadith["author"], reference=hadith["reference"])
     documents.append(Document(page_content=hadith["text"], metadata=metadata))
 
-sbert = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2")
+sbert = HuggingFaceEmbeddings(model_name="sentence-transformers/msmarco-bert-base-dot-v5")
 vectordb = Chroma(
   embedding_function = sbert,
-  persist_directory = 'chroma_store_scraper'
+  persist_directory = 'chroma_store_scraper_msmarco_bert_base_dot_v5'
 )
 vectordb.persist()
 
