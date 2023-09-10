6b16f113478724d64a8a75d3f24f9c8313fa7681
RaahimSiddiqi <k200146@nu.edu.pk>
2023-09-11 01:17:01 +0500

    Updated scraper output file

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 # Use this file for actually making the queries 
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceEmbeddings
+from pprint import pprint
+
+sbert = HuggingFaceEmbeddings(model_name="sentence-transformers/msmarco-bert-base-dot-v5")
+vectordb = Chroma(
+  embedding_function = sbert,
+  persist_directory = 'chroma_store_scraper_msmarco_bert_base_dot_v5'
+)
+
+print("searching...")
+results = vectordb.similarity_search_with_score("What is Qasr Prayer during travel?", k=10, search_type="hybrid")
+pprint(results)
